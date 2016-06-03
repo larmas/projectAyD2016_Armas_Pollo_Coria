8bb60e4f764346c88ab16181ff69ce3ecf4a38c3
Mariano Coria <mariano_coria95@hotmail.com>
2016-06-03 01:57:08 -0300

    everything working, still lacking details on falta envido

--- models/game.js
+++ models/game.js
@@ -15,64 +15,49 @@ var Player = playerModel.player;
 var Round  = roundModel.round;
 
 function Game(player1, player2){
-  /*
-   * Player 1
-   */
-  this.player1 = new Player("player 1");
+	//Player 1
+	this.player1 = new Player("player 1");
 
-  /*
-   * Player 2
-   */
-  this.player2 = new Player("player 2");
+  	// Player 2
+  	this.player2 = new Player("player 2");
 
-  /*
-   * sequence of previous Rounds
-   */
-  
-  this.rounds = [];
+  	// sequence of previous Rounds
+  	this.rounds = [];
 
-  /*
-   * Game's hand
-   */
-  this.currentHand = this.player1;
+  	//Game's hand
+  	this.currentHand = this.player1;
 
-  /*
-   * Game's hand
-   */
-  this.currentRound = undefined;
+  	//Game's hand
+  	this.currentRound = undefined;
 
-  /*
-   * Game' score
-   */
-  this.score = [0, 0];
+  	//Game's score
+  	this.score = [0, 0];
 }
 
-/*
- * Check if it's valid move and play in the current round
- */
+/* Check if it's valid move and play in the current round */
 Game.prototype.play = function(player, action, value){
 	if(this.currentRound.currentTurn !== player){
 		throw new Error("[ERROR] INVALID TURN...");}
 
-	if(this.currentRound.fsm.cannot(action)){
-		throw new Error("[ERROR] INVALID MOVE...");}
+	if (this.currentRound.fsm.current=='init' || action!='play-card'){
+		if(this.currentRound.fsm.cannot(action)){
+			throw new Error("[ERROR] INVALID MOVE...");
+		}
+	}
+		
 
-	this.currentRound.play(action, value,player);
+	return this.currentRound.play(action, value,player);
 };
 
 Game.prototype.changeHand = function(){
 	if(this.currentHand==this.player1){
 		return this.player2;
-	}
-	else{
+	}else{
 		return this.player1;
 	}
 }
 
-
-/*
- * Create and return a new Round to this game
- */
+/* Create and return a new Round to this game */
 Game.prototype.newRound = function(){
 	var round = new Round(this, this.currentHand);
 	this.currentRound = round;
@@ -81,12 +66,15 @@ Game.prototype.newRound = function(){
 	return this;
 }
 
-function noCL(){
-	return (this.player1.cards==[]) && (this.player2.cards==[]);
+function StartGame(){
+	while((this.score[0]<30) && (this.score[1]<30)){
+		var aux = this.player1;
+		while(this.currentRound.status='running'){
+			prompt(action,value);
+			aux = this.play(aux,action,value);
+		}
+		this.newRound();
+	}
 }
-
-/*
- * returns the opossite player
- */
 
 module.exports.game = Game;
--- models/round.js
+++ models/round.js
@@ -14,35 +14,64 @@ var cardModel = require('./card');
 var Card = cardModel.card;
 
 function newTrucoFSM(){
-  var fsm = StateMachine.create({
-  initial: 'init',
-  events: [
-    { name: 'play-card', 	from: 'init',     					to: 'first-card' },
-    { name: 'envido',    	from: ['init', 'first-card'],		to: 'envido' },
-    { name: 'quiero-e',    	from: ['envido'],        			to: 'quiero-e'  },
-    { name: 'no-quiero-e',  from: ['envido'],              		to: 'no-quiero-e'  },
-    { name: 'truco',     	from: ['init', 'first-card',
-                         	       'played-card','quiero-e',
-                         	       'no-quiero-e'],      		to: 'truco'  },
-    { name: 'quiero-t', 	from: ['truco'],              		to: 'quiero-t' },
-    { name: 'no-quiero-t', 	from: ['truco'],              		to: 'no-quiero-t' },
-    { name: 'play-card', 	from: ['quiero-e', 'no-quiero-e',
-                         	       'first-card', 'played-card'],to: 'played-card' },
-    { name: 'play-card', 	from: ['quiero-t'],              	to: 'quiero-t' }
-  ]});
+	var fsm = StateMachine.create({
+		initial: 'init',
+		events: 
+			[{ name: 'play-card', 		from: 'init',     					to: 'first-card' },
+			 { name: 'envido',    		from: ['init', 'first-card'],		to: 'envido' },
+			 { name: 'envido',    		from: ['envido'],					to: 'envido-e' },
+			 { name: 'real-envido',		from: ['init', 'first-card'],		to: 'real-envido' },
+			 { name: 'real-envido',    	from: ['envido'],					to: 'envido-re' },
+			 { name: 'real-envido',    	from: ['envido-e'],					to: 'envido-e-re' },
+			 { name: 'f-envido',    	from: ['init', 'first-card',
+			                        	       'envido','real-envido',
+			                        	       'envido-e','envido-re',
+			                        	       'envido-e-re'],				to: 'f-envido'},
+			 
+			 { name: 'quiero-e',    	from: ['envido'],        			to: 'quiero-e'  },
+			 { name: 'quiero-e-e',    	from: ['envido-e'],        			to: 'quiero-e-e'  },
+			 { name: 'quiero-re',    	from: ['real-envido'],        		to: 'quiero-re'  },
+			 { name: 'quiero-e-re',    	from: ['envido-re'],        		to: 'quiero-e-re'  },
+			 { name: 'quiero-e-e-re',  	from: ['envido-e-re'],     			to: 'quiero-e-e-re'  },
+			 { name: 'quiero-fe',    	from: ['f-envido'],        			to: 'quiero-fe'  },
+			 
+			 { name: 'no-quiero-e',    	from: ['envido'],        			to: 'no-quiero-e'  },
+			 { name: 'no-quiero-e-e',  	from: ['envido-e'],        			to: 'no-quiero-e-e'  },
+			 { name: 'no-quiero-re',   	from: ['real-envido'],        		to: 'no-quiero-re'  },
+			 { name: 'no-quiero-e-re', 	from: ['envido-re'],        		to: 'no-quiero-e-re'  },
+			 { name: 'no-quiero-e-e-re',from: ['envido-e-re'],     			to: 'no-quiero-e-e-re'  },
+			 { name: 'no-quiero-fe',    from: ['f-envido'],        			to: 'no-quiero-fe'  },
 
-  return fsm;
+			 { name: 'truco',     		from: ['init', 'first-card',
+			                      		       'played-card','quiero-e',
+			                      		       'no-quiero-e','quiero-e-e',
+			                      		       'quiero-re','quiero-e-re',
+			                      		       'quiero-e-e-re','quiero-fe',
+			                      		       'no-quiero-e-e','no-quiero-re',
+			                      		       'no-quiero-e-re','no-quiero-e-e-re',
+			                      		       'no-quiero-fe'],      		to: 'truco'  },
+			 { name: 'quiero-t', 		from: ['truco'],              		to: 'quiero-t' },
+			 { name: 'no-quiero-t', 	from: ['truco'],              		to: 'no-quiero-t' },
+			 { name: 'retruco', 		from: ['truco','quiero-t'],    		to: 'retruco' },
+			 { name: 'no-quiero-rt', 	from: ['retruco'],              	to: 'no-quiero-rt' },
+			 { name: 'quiero-rt', 		from: ['retruco'],              	to: 'quiero-rt' },
+			 { name: 'vale4', 			from: ['retruco','quiero-rt'],      to: 'vale4' },
+			 { name: 'no-quiero-v4', 	from: ['vale4'],              		to: 'no-quiero-v4' },
+			 { name: 'quiero-v4', 		from: ['vale4'],	              	to: 'quiero-v4' },
+			 
+		]});
+	return fsm;
 }
 
 function newPlayedFSM(){
-	  var fsm = StateMachine.create({
-	  initial: 'init',
-	  events: [
-	     { name: 'play-card', 	from: 'init',     				to: 'first-card' },
+	var fsm = StateMachine.create({
+	initial: 'init',
+	events: 
+		[{ name: 'play-card', 	from: 'init',     				to: 'first-card' },
 	     
-	     { name: 'play-card-w', from: 'first-card',				to: 'p2-won-1st'  },
-	     { name: 'play-card-s', from: 'first-card',				to: 'pardas'  },
-	     { name: 'play-card-l', from: 'first-card',				to: 'p1-won-1st'  },
+		 { name: 'play-card-w', from: 'first-card',				to: 'p2-won-1st'  },
+		 { name: 'play-card-s', from: 'first-card',				to: 'pardas'  },
+		 { name: 'play-card-l', from: 'first-card',				to: 'p1-won-1st'  },
 	     
 	     { name: 'play-card', 	from: 'p2-won-1st',				to: 'third-card-2'  },
 	     { name: 'play-card', 	from: ['pardas',
@@ -53,48 +82,46 @@ function newPlayedFSM(){
 	     { name: 'play-card-s', from: 'third-card-2',			to: 'p2-wins'  },
 	     { name: 'play-card-l', from: 'third-card-2',			to: 'p2-wins'  },
 	     { name: 'play-card-w', from: 'definitive-hand',		to: 'p2-wins'  },
-	     { name: 'play-card-s', from: 'definitive-hand',		to: 'definitive-hand'  },
+	     { name: 'play-card-s', from: 'definitive-hand',		to: 'pardas-2'  },
 	     { name: 'play-card-l', from: 'definitive-hand',		to: 'p1-wins'  },
 	     { name: 'play-card-w', from: 'third-card-1',			to: '1vs1-1'  },
 	     { name: 'play-card-s', from: 'third-card-1',			to: 'p1-wins'  },
 	     { name: 'play-card-l', from: 'third-card-1',			to: 'p1-wins'  },
 	     
 	     { name: 'play-card', 	from: '1vs1-2',				 	to: 'fifth-card-2'  },
+	     { name: 'play-card', 	from: 'pardas-2',				to: 'fifth-card-1'  },
 	     { name: 'play-card', 	from: '1vs1-1',				 	to: 'fifth-card-1'  },
 	     
 	     { name: 'play-card-w', from: 'fifth-card-2',			to: 'p2-wins'  },
 	     { name: 'play-card-s', from: 'fifth-card-2',			to: 'p2-wins'  },
-	     { name: 'play-card-l', from: 'third-card-2',			to: 'p1-wins'  },
+	     { name: 'play-card-l', from: 'fifth-card-2',			to: 'p1-wins'  },
 	     { name: 'play-card-w', from: 'fifth-card-1',			to: 'p1-wins'  },
 	     { name: 'play-card-s', from: 'fifth-card-1',			to: 'p1-wins'  },
-	     { name: 'play-card-l', from: 'third-card-1',			to: 'p2-wins'  },
-	     
-	  ]});
-
-	  return fsm;
-	}
-
-function Round(game,turn){
-  //Game
-  this.game = game;
-  
-  //next turn
-  this.currentTurn = turn;
-
-  //here is a FSM to perform user's actions
-  this.fsm = newTrucoFSM();
-  
-  this.fsmCP = newPlayedFSM();
-
-  this.auxCard = undefined;
-  
-  //Round' score
-  this.score = [0, 0];
+	     { name: 'play-card-l', from: 'fifth-card-1',			to: 'p2-wins'  },	     
+	]});
+	return fsm;
 }
 
-/*
- * Generate a new deck mixed and gives to players the correspondent cards
- */
+function Round(game,turn){
+	//Game
+	this.game = game;
+  
+	//next turn
+	this.currentTurn = turn;
+
+	//FSMs to perform user's actions
+	this.fsm = newTrucoFSM();
+	this.fsmCP = newPlayedFSM();
+  
+	this.auxCard = undefined;
+  
+	//Round's score
+	this.score = [0, 0];
+	
+	this.status='running';
+}
+
+/* Generates a new deck shuffled and gives to players the correspondent cards */
 Round.prototype.deal = function(){
 	var deck = new Deck().mix();
   	this.game.player1.setCards(_.pullAt(deck, 0, 2, 4));
@@ -103,15 +130,14 @@ Round.prototype.deal = function(){
 
 Round.prototype.changeTurn = function(game){
 	if (this.fsmCP.current=='p2-won-1st' || this.fsmCP.current=='1vs1-1' || this.fsmCP.current=='1vs1-2'){
-		this.currentTurn;
+		return this.currentTurn;
 	}
 	else{
-		this.currentTurn = this.switchPlayer(this.currentTurn,game);
+		return this.currentTurn = this.switchPlayer(this.currentTurn,game);
 	}
 }
-/*
- * returns the opossite player
- */
+
+/* returns the opposite player */
 Round.prototype.switchPlayer=function (player,game) {
 	if(player==game.player1){
 		return game.player2;
@@ -121,29 +147,41 @@ Round.prototype.switchPlayer=function (player,game) {
 	}
 };
 
-Round.prototype.calculateScore = function(action,fsm,fsmCP){
-	if(action == 'quiero-e'){
-		if (this.game.player1.envidoPoints<this.game.player2.envidoPoints){
-			this.score = [0, 2];
-		}else{
-			this.score = [2, 0];
-		}
-	}
-	else if (action=='no-quiero-e'){this.score[1,0];}
-	else if	(action=='no-quiero-t'){this.score[1,0];}
+Round.prototype.calculateScore = function(action,fsm,fsmCP,currentTurn){
+	aux  = (currentTurn==this.game.player1);
+	aux2 = (this.game.currentHand==this.game.player1);
+	auxEn =	this.game.player1.envidoPoints<this.game.player2.envidoPoints;
+	x=30-this.game.score[0];//puntos faltan j1
+	y=30-this.game.score[1];//puntos faltan j2
+	if(action == 'quiero-e')				{if(auxEn){this.score=[0,2];} else{this.score=[2,0];}}
+	else if (action=='quiero-e-e')			{if(auxEn){this.score=[0,4];} else{this.score=[4,0];}}
+	else if (action=='quiero-re')			{if(auxEn){this.score=[0,3];} else{this.score=[3,0];}}
+	else if (action=='quiero-e-re')			{if(auxEn){this.score=[0,5];} else{this.score=[5,0];}}
+	else if (action=='quiero-e-e-re')		{if(auxEn){this.score=[0,7];} else{this.score=[7,0];}}
+	else if (action=='quiero-fe')			{if(auxEn){this.score=[0,x];} else{this.score=[y,0];}}	
+	
+	else if (action=='no-quiero-e')			{if(auxEn){this.score=[0,1];} else{this.score=[1,0];}}
+	else if (action=='no-quiero-e-e')		{if(auxEn){this.score=[0,2];} else{this.score=[2,0];}}
+	else if (action=='no-quiero-re')		{if(auxEn){this.score=[0,1];} else{this.score=[1,0];}}
+	else if (action=='no-quiero-e-re')		{if(auxEn){this.score=[0,2];} else{this.score=[2,0];}}
+	else if (action=='no-quiero-e-e-re')	{if(auxEn){this.score=[0,3];} else{this.score=[3,0];}}
+	else if (action=='no-quiero-fe')		{if(auxEn){this.score=[0,1];} else{this.score=[1,0];}}
+	
+	else if (action=='no-quiero-t')		{if(aux){this.score=[0,1];} else{this.score=[1,0];}}
+	else if (action=='no-quiero-rt')	{if(aux){this.score=[0,2];} else{this.score=[2,0];}}
+	else if (action=='no-quiero-v4')	{if(aux){this.score=[0,3];} else{this.score=[3,0];}}
+	
 	else if (action=='play-card'){
 		if(fsmCP.current=='p1-wins'){
-			if(fsm.current=='quiero-t'){
-				this.score = [2, 0];
-			}else{
-				this.score = [1, 0];
-			}
+			if(fsm.current=='quiero-t')			{if(aux2){this.score = [2, 0];}else{this.score = [0, 2];}}
+			else if(fsm.current=='quiero-rt')	{if(aux2){this.score = [3, 0];}else{this.score = [0, 3];}}
+			else if(fsm.current=='quiero-v4')	{if(aux2){this.score = [4, 0];}else{this.score = [0, 4];}}
+			else 								{if(aux2){this.score = [1, 0];}else{this.score = [0, 1];}}
 		}else if(fsmCP.current=='p2-wins'){
-			if(fsm.current=='quiero-t'){
-				this.score = [0, 2];
-			}else{
-				this.score = [0, 1];
-			}
+			if(fsm.current=='quiero-t')			{if(aux2){this.score = [0, 2];}else{this.score = [2, 0];}}
+			else if(fsm.current=='quiero-rt')	{if(aux2){this.score = [0, 3];}else{this.score = [3, 0];}}
+			else if(fsm.current=='quiero-v4')	{if(aux2){this.score = [0, 4];}else{this.score = [4, 0];}}
+			else 								{if(aux2){this.score = [0, 1];}else{this.score = [1, 0];}}
 		}
 	}
 	
@@ -154,8 +192,10 @@ Round.prototype.calculateScore = function(action,fsm,fsmCP){
 }
 
 function makePlay(action,i,fsm,fsmCP,currentTurn){
-	fsm[action]();
 	if(action=='play-card'){
+		if(fsm.current=='init'){
+			fsm[action]();
+		}
 		if (this.auxCard == undefined){
 			fsmCP['play-card']();
 			this.auxCard = currentTurn.cards[i];
@@ -173,20 +213,33 @@ function makePlay(action,i,fsm,fsmCP,currentTurn){
 			this.auxCard = undefined;
 		}
 		delete currentTurn.cards[i];
+	}else{
+		fsm[action]();
 	}
 }
 
-/*
- * Let's Play :)
- */
+Round.prototype.checkStatus= function(fsmCP){
+	if ((fsmCP.current=='p1-wins') || (fsmCP.current=='p2-wins') || (fsmCP.current=='no-quiero-t') ||
+		(fsmCP.current=='no-quiero-rt') || (fsmCP.current=='no-quiero-v4') ){
+		this.status='finished';
+	}
+};
+
+/********************************************************/
 Round.prototype.play = function(action, value,player) {
-	// move to the next state
+	// move to next states
 	makePlay(action,value,this.fsm,this.fsmCP,player);
+	
 	// check if is needed sum score
 	this.calculateScore(action, this.fsm, this.fsmCP);
 
+	//check status
+	this.checkStatus(this.fsmCP);
+	
 	// Change player's turn
-	this.changeTurn(this.game);
+	return this.changeTurn(this.game);
+	
+	
 };
 
 module.exports.round = Round;
